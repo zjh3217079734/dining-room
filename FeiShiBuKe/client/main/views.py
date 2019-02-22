7a32bf823b68c081dd29a40ee3c665f21304a2ad
liu <1543926437@qq.com>
2019-02-22 20:33:21 +0800

    shops

--- FeiShiBuKe/client/main/views.py
+++ FeiShiBuKe/client/main/views.py
@@ -270,3 +270,42 @@ def search_views():
                 l.append(r[0])
     jsonStr = json.dumps(l)
     return jsonStr
+
+#------------------------------------------------------------
+#刘光辉 商品分类
+@main.route('/goods')
+def goods_views():
+    shop_id = request.args['shop_id']
+    shop = Shop.query.filter_by(id=shop_id).first()
+    #商品分类列表
+    menus = shop.shop_meun #Menu.query.filter_by(shop_id=shop_id).all()
+    #所有商品列表
+    l = []
+    # if request.args['goods_type']:
+    #    l.append(request.args['goods_type'])
+    # else:
+    for menu in menus:
+        l.append(menu.id)
+    # 商品分页
+    pageSize = 9
+    page = request.args.get('page','1')
+    page = int(page)
+
+    ost = (page-1) * pageSize
+    goods = db.session.query(Goods).filter(Goods.menu_id.in_(l),Goods.goods_status==1).limit(pageSize).offset(ost).all()
+    # 对应商店里能显示的商品总数
+    totalCount = db.session.query(Goods).filter(Goods.menu_id.in_(l),Goods.goods_status==1).count()
+    # 最后一页页码
+    lastPage = math.ceil(totalCount / pageSize)
+    # 设置上一页默认为 1
+    prevPage = 1
+    if page > 1:
+        prevPage = page - 1
+    
+    nextPage = lastPage
+    if page < lastPage:
+        nextpage = page +1
+
+    return render_template('/shop.html',params=locals())
+#-------------------------------------------------------------------
+
