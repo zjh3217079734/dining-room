48fbff25f0abc8c2e5acfbb36aa7bc953b7060d5
Kate <44628211+Kate2018search@users.noreply.github.com>
2019-02-23 23:55:30 +0800

    注册/登录/修改密码/功能继续完善

--- FeiShiBuKe/client/main/views.py
+++ FeiShiBuKe/client/main/views.py
@@ -48,24 +48,71 @@ def SelectCity():
 # -----------------------------------------------------------
 # 应巧
 
-
 @main.route('/')
 def index_views():
     if 'username' in request.cookies:
         username = request.cookies['username']
         return render_template('index.html', params=locals())
+    elif 'username' in session:
+        username = session['username']
+        return render_template('index.html', params=locals())
     else:
-        return render_template('index.html', params=None)
+        return render_template('index.html',params={})
 
 
-@main.route('/login', methods=['POST', 'GET'])
+@main.route('/register', methods=['GET', 'POST'])
+def register_views():
+    if request.method == 'GET':
+        return render_template('login-register.html', params={})
+    else:
+        username = request.form['uname']
+        upwd = request.form['upwd']
+        s = sha1()
+        s.update(upwd.encode())
+        password = s.hexdigest()
+        phone = request.form['uphone']
+
+        user = User()
+        user.user_name = username
+        user.password = password
+        user.phone = phone
+        user.sex = 'M'
+        user.create_time = datetime.now()
+        user.update_time = datetime.now()
+
+        try:
+            db.session.add(user)
+            db.session.commit()
+            return render_template('index.html', params=locals())
+        except Exception as ex:
+            print(ex)
+            return "注册失败，请联系管理员！"
+
+
+@main.route('/logout')
+def logout_views():
+    url = request.headers.get('Referer', '/')
+    # print(url)
+    resp = redirect(url)
+    # print("resp:",resp)
+    # print(request.cookies)
+    if 'username' in request.cookies:
+        resp.delete_cookie('username')
+    elif 'username'in session:
+        session.clear()
+        # print(request.cookies)
+    return resp
+
+
+@main.route('/login', methods=['GET', 'POST'])
 def login_views():
     if request.method == 'GET':
         url = request.headers.get('Referer', '/')
         session['url'] = url
-        # return render_template('login-register.html')
         if 'username' in session:
             return redirect(url)
+            username = session['username']
+            return render_template('index.html', params=locals())
         else:
             if 'username' in request.cookies:
                 username = request.cookies['username']
@@ -84,64 +131,59 @@ def login_views():
                 return render_template('login-register.html', params={})
     else:
         username = request.form['username']
-        password = request.form['password']
+        upwd = request.form['password']
+        s = sha1()
+        s.update(upwd.encode())
+        password = s.hexdigest()
         user = User.query.filter_by(
             user_name=username, password=password).first()
+        # print(user)
+        # print(user.user_id)
         if user:
             session['id'] = user.user_id
             session['username'] = username
             url = session['url']
+            # print(url)
             resp = redirect(url)
+            # print(resp)
             if 'isSaved' in request.form:
                 resp.set_cookie('username', username, 60 * 60 * 24 * 365 * 10)
             return resp
         else:
             errMsg = "用户名或密码不正确"
-            return render_template('login-register.html', params=locals())
+            resp=make_response(render_template('login-register.html', params=locals()))
+            resp.delete_cookie('username')
+            return resp
 
 
-@main.route('/register', methods=['GET', 'POST'])
-def register_views():
+@main.route('/resetpwd', methods=['GET', 'POST'])
+def resetpwd_views():
     if request.method == 'GET':
-        return render_template('login-register.html', params={})
+        # print(1)
+        return render_template('ResetPwd.html', params={})
     else:
-        username = request.form['uname']
-        password = request.form['upwd']
-        phone = request.form['uphone']
+        username=request.form['username']
+        # print(username)
+        npwd=request.form['newpassword']
+        # print(npwd)
 
-        user = User()
-        user.user_name = username
-        user.password = password
-        user.phone = phone
-        user.sex = 'M'
-        user.create_time = datetime.now()
-        user.update_time = datetime.now()
-        try:
+        s=sha1()
+        s.update(npwd.encode())
+        password=s.hexdigest()
+        number=request.form['number']
+        user=User.query.filter_by(user_name=username,phone=number).first()
+        # print(user)
+        if user:
+            user.password=password
+            user.create_time=datetime.now()
+            user.update_time=datetime.now()
+
             db.session.add(user)
             db.session.commit()
-            return render_template('index.html', params=locals())
-        except Exception as ex:
-            print(ex)
-            return "注册失败，请联系管理员！"
+            return "密码修改成功，请重新登录"
+        else:
+            return "用户名和手机号不匹配，请重试"
 
-
-@main.route('/logout')
-def logout_views():
-    url = request.headers.get('Referer', '/')
-    resp = redirect(url)
-    if 'username' in request.cookies:
-        resp.delete_cookie('username')
-    return resp
-
-
-# @main.route('/register/checkuname')
-# def checkuname():
-#     uname = request.args['uname']
-#     users = User.query.filter_by(user_name=uname).all()
-#     if users:
-#         return "1"
-#     else:
-#         return "0"
 
 # -----------------------------------------------------------
 
